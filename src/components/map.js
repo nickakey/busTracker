ba2fd8c6df6b23d96e2708dbf71057a5872480da
Nick Akey <nick@nickakey.com>
2017-10-03 15:51:14 -0700

    directive is building map

--- src/components/map.js
+++ src/components/map.js
@@ -9,7 +9,10 @@ angular.module('app')
 .directive('map', function(geoJSON){
   return {
     link: function($scope, element){
-
+      geoJSON.parseJSON()
+      .then((geoData)=>{this.draw(element, geoData)})
+    },
+    draw(element, geoData){ 
       var ctx = element[0].firstChild.getContext('2d')
 
       var height = window.innerHeight;
@@ -19,23 +22,18 @@ angular.module('app')
       .center([-122.4194, 37.7749])
       .scale(300000)
       .translate([width / 2, height / 2])
+      var neighborhoods = geoData.data.neighborhoods;
 
-      geoJSON.parseJSON()
-      .then((data)=>{
-        console.log('this is the data! ', data)
-        var neighborhoods = data.data.neighborhoods;
-
-        var path = d3.geo.path()
-        .projection(projection)
-        .context(ctx)      
-        ctx.beginPath()
-        path(neighborhoods)
-        ctx.fillStyle = '#dcd8d2'
-        ctx.fill()
-        ctx.lineWidth = '2'
-        ctx.strokeStyle = '#c9c4bc'
-        ctx.stroke()  
-      })
+      var path = d3.geo.path()
+      .projection(projection)
+      .context(ctx)      
+      ctx.beginPath()
+      path(neighborhoods)
+      ctx.fillStyle = '#dcd8d2'
+      ctx.fill()
+      ctx.lineWidth = '2'
+      ctx.strokeStyle = '#c9c4bc'
+      ctx.stroke()        
     },
     templateUrl: 'src/templates/map.html',
     // controller: 'mapController'
