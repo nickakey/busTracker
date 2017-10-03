be27b749e1f27b54570b4cab84c9702777f3e7d8
Nick Akey <nick@nickakey.com>
2017-10-03 15:45:57 -0700

    Basic Map Drawing is Working

--- src/components/map.js
+++ src/components/map.js
@@ -1,21 +1,43 @@
 angular.module('app')
-.controller('mapController', function($scope, geoJSON){
-  geoJSON.parseJSON()
-  .then((geoJSON)=>{
-    $scope.geoJSON = geoJSON;
-  })
-})
+// .controller('mapController', function($scope, geoJSON){
+//   geoJSON.parseJSON()
+//   .then((geoJSON)=>{
+//     $scope.geoJSON = geoJSON;
+//   })
+// })
 
-.directive('map', function(){
+.directive('map', function(geoJSON){
   return {
     link: function($scope, element){
-      var ctx = element[0].firstChild.getContext('2d')
-      console.log($scope)
-      // ctx.strokeStyle="#FF0000";
-      // ctx.strokeRect(20,20,150,100);
 
+      var ctx = element[0].firstChild.getContext('2d')
+
+      var height = window.innerHeight;
+      var width = window.innerWidth;
+
+      var projection = d3.geo.mercator()
+      .center([-122.4194, 37.7749])
+      .scale(300000)
+      .translate([width / 2, height / 2])
+
+      geoJSON.parseJSON()
+      .then((data)=>{
+        console.log('this is the data! ', data)
+        var neighborhoods = data.data.neighborhoods;
+
+        var path = d3.geo.path()
+        .projection(projection)
+        .context(ctx)      
+        ctx.beginPath()
+        path(neighborhoods)
+        ctx.fillStyle = '#dcd8d2'
+        ctx.fill()
+        ctx.lineWidth = '2'
+        ctx.strokeStyle = '#c9c4bc'
+        ctx.stroke()  
+      })
     },
     templateUrl: 'src/templates/map.html',
-    controller: 'mapController'
+    // controller: 'mapController'
   }
 })
